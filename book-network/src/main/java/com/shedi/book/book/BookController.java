e7dbe9f11a2f45693bdd625aa3274413e6de5fe3
shedi amara nebli <96447936+Sheedii@users.noreply.github.com>
2025-01-31 11:04:25 +0100

    README.md

--- book-network/src/main/java/com/shedi/book/book/BookController.java
+++ book-network/src/main/java/com/shedi/book/book/BookController.java
@@ -92,11 +92,20 @@ public class BookController {
     }
 
     @PatchMapping("/borrow/return/{book-id}")
-    public ResponseEntity<Integer> returnBorrowed(
+    public ResponseEntity<Integer> returnBorrow(
             @PathVariable("book-id") Integer bookId,
             Authentication connectedUser
     ){
         return ResponseEntity.ok(service.returnBorrowedBook(bookId, connectedUser));
     }
+
+    @PatchMapping("/borrow/return/approve/{book-id}")
+    public ResponseEntity<Integer> approveReturnBorrow(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.approveReturnBorrowedBook(bookId, connectedUser));
+    }
+
 
 }
